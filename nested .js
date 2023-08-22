357e2717bd723f2527ae5e88263cfb9dcfc5d0f6
prajwaln25 <134259488+prajwaln25@users.noreply.github.com>
2023-08-22 08:55:19 +0530

    Add files via upload

---
+++ nested .js
@@ -0,0 +1,22 @@
+const person ={
+    fristName : "john",
+    lastName :"Doe",
+    age :30,
+    address :{
+        street:"123 Main Street",
+        city :"New York ",
+        state :"NY",
+        zipcode: "10001"
+    },
+    occupation :"Engineer"
+};
+    console.log(person.fristName);
+    console.log(person.lastName);
+    console.log(person.age);
+    console.log(person.occupation);
+    console.log(person.address.street);
+    console.log(person.address.city);
+    console.log(person.address.state);
+    console.log(person.address.zipcode);
+
+
